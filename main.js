b3b2e357d54475fdb6a1a1ddc9857921bc95381a
Aloha Churchill <aloha_churchill@trimble.com>
2023-08-29 13:32:40 -0600

    adding more circlez

--- main.js
+++ main.js
@@ -27,8 +27,29 @@ function createCircle(radius, position, color=0xffffff) {
 }
 
 
-const initialCircle = createCircle(radius, { x: 0, y: 0, z: 0 });
+function solveCircles(x1, y1, r1, x2, y2, r2, x3, y3, r3, r4) {
+    // Calculate the coefficients of the equations
+    const A1 = 2 * (x2 - x1);
+    const B1 = 2 * (y2 - y1);
+    const C1 = x2*x2 - x1*x1 + y2*y2 - y1*y1 + (r1 + r4)*(r1 + r4) - (r2 + r4)*(r2 + r4);
+    
+    const A2 = 2 * (x3 - x1);
+    const B2 = 2 * (y3 - y1);
+    const C2 = x3*x3 - x1*x1 + y3*y3 - y1*y1 + (r1 + r4)*(r1 + r4) - (r3 + r4)*(r3 + r4);
+    
+    // Use the elimination method to solve for a and b
+    const det = A1*B2 - A2*B1;
+    
+    if (det === 0) {
+        throw new Error("No unique solution exists.");
+    }
+    
+    const a = (C1 * B2 - C2 * B1) / det;
+    const b = (A1 * C2 - A2 * C1) / det;
 
+    // return a point specificed where a is the x coord and b is the y coord
+    return {x: a, y: b, z: 0};
+}
 
 function generateBaseCircles() {
     // Generate three random points in the window
@@ -52,14 +73,32 @@ function generateBaseCircles() {
         }
     }
 
-    
+    let radius1 = (distances[0] + distances[1] - distances[2])/2;
+    let radius2 = (distances[0] + distances[2] - distances[1])/2;
+    let radius3 = (distances[1] + distances[2] - distances[0])/2;
 
-    // Draw three tangent circles with the three points
-    for (let i = 0; i < 3; i++) {
-        const point = points[i];
-        createCircle(radius, point);
-    }
+    // Draw three tangent circles with the three centroid points and corresponding radii
+    createCircle(radius1, points[0]);
+    createCircle(radius2, points[1]);
+    createCircle(radius3, points[2]);
+
+
+    // Find two circles tangent to the first three using Descartes' Theorem
+    let radius4 = 1/(1/radius1 + 1/radius2 + 1/radius3 + 2*Math.sqrt(1/radius1*1/radius2 + 1/radius2*1/radius3 + 1/radius3*1/radius1));
+    let radius5 = 1/(1/radius1 + 1/radius2 + 1/radius3 - 2*Math.sqrt(1/radius1*1/radius2 + 1/radius2*1/radius3 + 1/radius3*1/radius1));
+
+    console.log(radius4);
+    let centroid4 = solveCircles(points[0].x, points[0].y, radius1, points[1].x, points[1].y, radius2, points[2].x, points[2].y, radius3, radius4); 
+    createCircle(radius4, centroid4);
+
+    console.log(radius5);
+    let centroid5 = solveCircles(points[0].x, points[0].y, radius1, points[1].x, points[1].y, radius2, points[2].x, points[2].y, radius3, radius5);
+    createCircle(radius5, centroid5);
+
 }
+
+generateBaseCircles();
+
 
 function animate() {
 	requestAnimationFrame( animate );
